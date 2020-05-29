50f5bfd7562b3531b43d58f9cf60e2a731b56d88
Henrique Abreu <abreu-semperfi@icloud.com>
2020-05-29 21:35:07 +0100

    Finished functions for all the dice TODO UI and UI constraints

--- RpgDice/ViewController.swift
+++ RpgDice/ViewController.swift
@@ -12,12 +12,46 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var resultLabel: UILabel!
     
-    @IBAction func rollD6(_ sender: Any) {
-        
-        let result = Int.random(in: 1 ... 6)
-        resultLabel.text = String(result)
+    func  rollDice(dice : Int) -> Int{
+        switch dice {
+        case 6:
+            return Int.random(in: 1 ... 6)
+        case 8:
+            return Int.random(in: 1 ... 8)
+        case 12:
+            return Int.random(in: 1 ... 12)
+        default:
+            return Int.random(in: 1 ... 20)
+        }
     }
     
+    func setLabelTxt(text : Int){
+        resultLabel.text = String(text)
+        
+    }
+    
+    @IBAction func rollD6(_ sender: Any) {
+        
+        let result = rollDice(dice: 6)
+        setLabelTxt(text: result)
+        
+    }
+    
+    @IBAction func rollD8(_ sender: Any) {
+        
+        let result = rollDice(dice: 8)
+        setLabelTxt(text: result)
+    }
+    
+    @IBAction func rollD12(_ sender: Any) {
+        let result = rollDice(dice: 12)
+               setLabelTxt(text: result)
+    }
+    
+    @IBAction func rollD20(_ sender: Any) {
+        let result = rollDice(dice: 20)
+               setLabelTxt(text: result)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
