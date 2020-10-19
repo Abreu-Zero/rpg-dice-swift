af21009f85fa3b2b550091fb9bce87e936e89ded
Henrique Abreu <abreu-semperfi@hotmail.com>
2020-10-19 18:58:12 +0100

    added: last UITest for the rolls
    used the same approach to check all the remaining rolls
    we can now start to test if all the Api dependent methods are running

--- RpgDiceUITests/RpgDiceUITests.swift
+++ RpgDiceUITests/RpgDiceUITests.swift
@@ -39,12 +39,44 @@ class RpgDiceUITests: XCTestCase {
         guard let roll = result else {return}
         XCTAssertTrue(1 <= roll && roll <= 6)
     }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-                XCUIApplication().launch()
-            }
-        }
+    
+    func testD8() throws {
+        app.buttons["Dice"].tap()
+        app.buttons["D8"].tap()
+        let result = Int(app.staticTexts.element(matching: .any, identifier: "resultLabelId").label)
+        guard let roll = result else {return}
+        XCTAssertTrue(1 <= roll && roll <= 8)
     }
+    
+    func testD10() throws {
+        app.buttons["Dice"].tap()
+        app.buttons["D10"].tap()
+        let result = Int(app.staticTexts.element(matching: .any, identifier: "resultLabelId").label)
+        guard let roll = result else {return}
+        XCTAssertTrue(1 <= roll && roll <= 10)
+    }
+    
+    func testD12() throws {
+        app.buttons["Dice"].tap()
+        app.buttons["D12"].tap()
+        let result = Int(app.staticTexts.element(matching: .any, identifier: "resultLabelId").label)
+        guard let roll = result else {return}
+        XCTAssertTrue(1 <= roll && roll <= 12)
+    }
+    
+    func testD20() throws {
+        app.buttons["Dice"].tap()
+        app.buttons["D20"].tap()
+        let result = Int(app.staticTexts.element(matching: .any, identifier: "resultLabelId").label)
+        guard let roll = result else {return}
+        XCTAssertTrue(1 <= roll && roll <= 20)
+    }
+
+//    func testLaunchPerformance() throws {
+//        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+//            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
+//                XCUIApplication().launch()
+//            }
+//        }
+//    }
 }
